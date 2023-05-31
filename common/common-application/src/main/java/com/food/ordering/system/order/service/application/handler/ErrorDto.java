d91d01aec146aefc6752f8a0537d3ba1fe54ed47
Sujith s <sujithsuresh05@gmail.com>
2023-05-31 13:13:23 +0400

    completed adding order controller class

---
+++ common/common-application/src/main/java/com/food/ordering/system/order/service/application/handler/ErrorDto.java
@@ -0,0 +1,15 @@
+package com.food.ordering.system.order.service.application.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ErrorDto {
+
+    private final String code;
+    private final String message;
+
+}
